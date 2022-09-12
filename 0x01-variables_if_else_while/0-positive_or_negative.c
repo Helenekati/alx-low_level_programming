e7e5e7cc848052c912cda60aa7b6f46c51960a39
Helenekati <ekatihelen@gmail.com>
2022-09-12 06:41:31 -0700

    postive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,15 +7,17 @@
 * Return: Always success
 */
 {
+
 int n;
 strand(time(0);
 n =rand()-RAND_MAX /2;f
 
-if (n>o)
+if (n > o)
 	printf("%d is positive\n",n);
+
    else if (n==o)
 	printf("%d is zero\n",n);
    else 
-	printf("%d is negetive\n",n);
+	printf("%d is negetive\n",n);	
      return(o);
 }  
