b3efad03a34b279e3a9fa1e16c61445b57904a2f
Helenekati <ekatihelen@gmail.com>
2022-09-22 00:25:29 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,24 +3,25 @@
 
 /**
  * _strcat-concatenates two strings
- * @dest:The destination string
- * @src:The source string
+ * @dest: The destination string
+ * @src: The source string
  *
  * Return: A pointer to the resuiting string dest
  */
+
 char *_strcat (char *dest, char *src)
 
 {
 	int dlen = 0, i;
-	while ( dest[dlen])
+	while (dest[dlen])
 	{
 		dlen++;
 	}
 	for (i = 0; src[i] !=0; l++)
 	{
-		dest[dlen] = src[i];
+		dest [dlen] = src[i];
 		dlen++;
 	}
-	dest[dlen] ='\0';
+	dest[dlen] = '\0';
 	return (dest);
 }
