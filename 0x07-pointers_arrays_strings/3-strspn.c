fcc99191c0ef82fb631258503b6b73a5c10a35ec
Helenekati <ekatihelen@gmail.com>
2022-09-26 21:07:31 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,8 +17,10 @@ unsigned int _strspn(char *s, char *accept)
 	f = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
+
 	{
 		flag = 0;
+
 		for (j = 0; accept[j] != '\0'; j++)
 														{
 															if (s[i] == accept[j])
