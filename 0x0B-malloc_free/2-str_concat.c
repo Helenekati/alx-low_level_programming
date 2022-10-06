4606441cc142d7d81f697602f5409264caab7150
Helenekati <ekatihelen@gmail.com>
2022-10-06 09:41:35 -0700

    Trust no one

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,8 +32,6 @@ char *str_concat(char *s1, char *s2)
 	for (index = 0; s1[index]; index++)
 		concat_str[concat_index++] = s1[index];
 
-	for (index = 0; s2[index]; index++)
-														concat_str[concat_index++] = s2[index];
-
+	for (index = 0; s2[index]; index++)  									concat_str[concat_index++] = s2[index]i;
 	return (concat_str);
 }
