5c24c01f813d68ce7e1c1e3e409d3eeab90b4184
Helenekati <ekatihelen@gmail.com>
2022-10-21 10:08:54 -0700

    Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,71 +1,40 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * add_node_end - function that adds a new node
- * at the end of a list_t list
- * @head: The main linked list
- * @str: string to add to the node
- *
- * Return: an address of the new list or NULL if it failed
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node_end(list_t **head,const char *str)
 
 {
-	list_t *new_list, *temp;
+	list_t *new, *temp;
+	size_t nchar;
+	
+	new = malloc(size of (list_t));
+	if (new == NULL)
+		return (NULL);
 
-	if (str != NULL)
+	new-str = strdup (str);
 
+	for (nchar = 0; str [nchar]; nchar++);
+
+	new->len =nchar;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
 	{
-		new_list = malloc(sizeof(list_t));
-
-		if (new_list == NULL)
-															return (NULL);
-													
-		new_list->str = strdup(str);
-
-		new_list->len = _strlen(str);
-
-		new_list->next = NULL;
-													if (*head == NULL)
-													{
-														*head  = new_list;
-														return (*head);
-													}
-													else
-													{
-														temp = *head;
-														while (temp->next;																							temp = temp->next;	
-																										temp->next = new_list;
-
-		return (temp);
-
+		*head = new;
 	}
-													}
-
-	return (NULL);
-}
-
-/**
- * _strlen - The length of a string
- * @s: A string to count
- *
- * Return: String length
- */
-int _strlen(const char *s)
-
-{
-	int c = 0;
-
-	while (*s)
-
+	else
 	{
-		s++;
-
-		c++;
+		while (temp->next !=NULL)
+			temp = temp->next;
+		temp->next = new;
 	}
 
-	return (c);
+	return (*head);
 }
