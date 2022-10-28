821fb4f0cb5a4ff6315b3324699208a0e4543d2f
Helenekati <ekatihelen@gmail.com>
2022-10-27 17:21:07 -0700

    Tasks 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,17 +19,17 @@ unsigned int binary_to_uint(const char *b)
 	for (place = 1, uint = 0; n >= 0; n--)
 	{
 		if (b[n] == '0')
-														{
+		{
+			place *= 2;
+															continue;
+														}
+		else if (b[n] == '1')
+		{
+			uint += place;
 															place *= 2;
 															continue;
 														}
-														else if (b[n] == '1')
-														{
-															uint += place;
-															place *= 2;
-															continue;
-														}
-														return (0);
-													}
+		return (0);
+	}
 	return (uint);
 }
